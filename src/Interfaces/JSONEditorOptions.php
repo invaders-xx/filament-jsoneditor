cfa1701506d63e74c35b1d7b76434b4010c78875
David Vincent <envahisseur@gmail.com>
2022-04-02 08:27:39 +0200

    Update JSONEditorOptions.php
    Fix Closure

--- src/Interfaces/JSONEditorOptions.php
+++ src/Interfaces/JSONEditorOptions.php
@@ -2,11 +2,13 @@
 
 namespace InvadersXX\FilamentJsoneditor\Interfaces;
 
+use Closure;
+
 interface JSONEditorOptions
 {
-    public function modes(array | Closure | null $modes): static;
+    public function modes(array|Closure|null $modes): static;
 
-    public function height(int | Closure | null $height): static;
+    public function height(int|Closure|null $height): static;
 
     public function getHeight(): ?int;
 
