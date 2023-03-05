70b29f33252819430e97084efc5e1a1ec3b0dd30
David Vincent <envahisseur@gmail.com>
2023-03-05 10:33:58 +0100

    Update ServiceProvider

--- src/FilamentJsoneditorServiceProvider.php
+++ src/FilamentJsoneditorServiceProvider.php
@@ -23,33 +23,16 @@ class FilamentJsoneditorServiceProvider extends PluginServiceProvider
         'invaders-filament-jsoneditor' => __DIR__ . '/../dist/jsoneditor/jsoneditor.min.css',
     ];
 
-    public function boot()
-    {
-        $this->bootLoaders();
-        $this->bootPublishing();
-    }
-
-    protected function bootPublishing()
-    {
-        if (! $this->app->runningInConsole()) {
-            return;
-        }
-
-        $this->publishes([
-            __DIR__ . '/../dist/jsoneditor/img/jsoneditor-icons.svg' => public_path('filament/assets/img/jsoneditor-icons.svg'),
-        ], 'filament-jsoneditor-img');
-    }
-
-    protected function bootLoaders()
-    {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-jsoneditor');
-    }
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name(self::$name)
             ->hasConfigFile()
+            ->hasAssets()
             ->hasViews();
+
+        $this->publishes([
+            __DIR__ . '/../dist/jsoneditor/img/jsoneditor-icons.svg' => public_path('filament/assets/img/jsoneditor-icons.svg'),
+        ], 'filament-jsoneditor-img');
     }
 }
